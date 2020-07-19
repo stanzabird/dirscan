3269422ed8ae94a0643ac97e077e22943cc45fc0
Bert van der Weerd <bert@stanzabird.nl>
2020-07-19 17:09:12 +0200

    made it work better in vs

--- src/dirscan_mt.cc
+++ src/dirscan_mt.cc
@@ -1,5 +1,9 @@
 #include "dirscan.h"
 
+// Our own attempt at n-level generalization of the windirstat algorithm.
+//
+// First we get all the stuff in this folder.
+// 
 int dirscan_info::dirscan_mt(const std::string& dirname)
 {
   return 0;
