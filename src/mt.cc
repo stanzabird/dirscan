b748ededd3a6d6e84c2f50f0888f07227f16c184
Bert van der Weerd <bert@stanzabird.nl>
2020-07-24 11:00:31 +0200

    trying some mt stuff

--- src/mt.cc
+++ src/mt.cc
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <future>
+#include <thread>
+
+
 int main(int argc, char* argv[]) {
+  auto n = std::thread::hardware_concurrency();
+  
+  std::cout
+    << n << " concurrent threads are supported.\n";
+  
   return 0;
 }
